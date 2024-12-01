35bc333372353ae1d7513343353ae1be7b5068e2
PerikiyoXD <perikiyoxd@gmail.com>
2024-12-01 11:08:59 +0100

    Make RW header pretty

--- rw.h
+++ rw.h
@@ -1,7 +1,11 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
+#ifndef RW_H
+#define RW_H
 
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+
+// Base includes
 #include "src/rwbase.h"
 #include "src/rwerror.h"
 #include "src/rwplg.h"
@@ -13,13 +17,25 @@
 #include "src/rwplugins.h"
 #include "src/rwuserdata.h"
 #include "src/rwcharset.h"
+
+// Platform-specific includes
+#ifdef __PS2__
 #include "src/ps2/rwps2.h"
 #include "src/ps2/rwps2plg.h"
+#endif
+
+#ifdef __D3D__
 #include "src/d3d/rwxbox.h"
 #include "src/d3d/rwd3d.h"
 #include "src/d3d/rwd3d8.h"
 #include "src/d3d/rwd3d9.h"
+#endif
+
+#ifdef __GL__
 #include "src/gl/rwwdgl.h"
 #include "src/gl/rwgl3.h"
 #include "src/gl/rwgl3shader.h"
 #include "src/gl/rwgl3plg.h"
+#endif
+
+#endif // RW_H
